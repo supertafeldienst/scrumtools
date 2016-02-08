77a80182b81588b2e38c2d06aec903ed28423769
supertafeldienst <GLiebermann@IT0013833.publicanet.de>
2016-02-08 18:09:23 +0100

    Test Commit

--- sprintback/src/main/java/com/weltbild/sprintback/SprintbackApplication.java
+++ sprintback/src/main/java/com/weltbild/sprintback/SprintbackApplication.java
@@ -3,6 +3,12 @@ package com.weltbild.sprintback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Root Klasse für die Application
+ * 
+ * @author GLiebermann
+ *
+ */
 @SpringBootApplication
 public class SprintbackApplication {
 
