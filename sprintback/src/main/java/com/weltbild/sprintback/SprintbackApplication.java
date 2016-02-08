11d2e770d3aa2ddd99469279a80e0b067ab5a1e5
supertafeldienst <GLiebermann@IT0013833.publicanet.de>
2016-02-08 17:55:16 +0100

    Test

---
+++ sprintback/src/main/java/com/weltbild/sprintback/SprintbackApplication.java
@@ -0,0 +1,12 @@
+package com.weltbild.sprintback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbackApplication.class, args);
+	}
+}
---
+++ sprintback/src/test/java/com/weltbild/sprintback/SprintbackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.weltbild.sprintback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SprintbackApplication.class)
+public class SprintbackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
